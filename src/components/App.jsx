f7ebbcc24e93dc7303409fdeb0834d297f0e0c80
Ed <silva.email@yahoo.com>
2024-02-29 16:27:19 -0500

    Fix formatting in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,20 @@ function App() {
   const [count, setCount] = useState(0);
 
   function increase() {
-    setCount (count + 1)
+    setCount(count + 1);
   }
 
   function decrease() {
-    setCount (count -1)
+    setCount(count - 1);
   }
 
   return (
-  <div className ="container">
-  <h1>{count}</h1>
-  <button onClick = {decrease}>-</button>
-  <button onClick = {increase}>+</button>
-</div>
-  )
-  }
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
 
 export default App;
