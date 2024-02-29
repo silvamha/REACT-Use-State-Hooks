d239c9e136d2adaafc5613a92520c46aad0b61e3
Ed <silva.email@yahoo.com>
2024-02-29 16:29:53 -0500

    Test commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById("root")
+<App 
+
+/>, 
+document.getElementById("root")
 );
